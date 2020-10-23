76702fc364ca28206d5476c274a0209a07f77777
amandaos <amanda.775.souza@gmail.com>
2020-10-23 20:16:21 -0300

    maior tres numeros

---
+++ exercises/bloco_4/dia_1/maiorTresNumeros.js
@@ -0,0 +1,11 @@
+let a = 75;
+let b = 332;
+let c = 64;
+
+if (a > b && a > c){
+    console.log(a + " É o maior número entre os três");
+} else if (b > a && b > c) {
+    console.log(b + " É o maior número entre os três");
+} else {
+    console.log(c + " É o maior número entre os três");
+}
