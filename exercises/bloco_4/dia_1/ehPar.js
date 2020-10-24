ef573c7399214ab706ac5001c860b084a3796338
amandaos <amanda.775.souza@gmail.com>
2020-10-23 21:49:21 -0300

    corrige o ehPar e adiciona ehImpar

--- exercises/bloco_4/dia_1/ehPar.js
+++ exercises/bloco_4/dia_1/ehPar.js
@@ -1,12 +1,12 @@
-let a = 13;
+let a = 14;
 let b = 19;
 let c = 35;
 let moduloa = (a % 2 == 0);
 let modulob = (b % 2 == 0);
 let moduloc = (c % 2 == 0);
+let ehPar = false;
 
 if (moduloa || modulob || moduloc){
-    console.log("um dos três numeros é par");
-} else {
-    console.log("nenhum numero é par")
+    ehPar = true;
 }
+console.log(ehPar)
