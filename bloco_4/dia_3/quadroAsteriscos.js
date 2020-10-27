d3b8f426e14a40ef09bb9f8600adc8d60dc7fb19
amandaos <amanda.775.souza@gmail.com>
2020-10-27 19:10:07 -0300

    começando um novo dia com mais exercicios

---
+++ bloco_4/dia_3/quadroAsteriscos.js
@@ -0,0 +1,11 @@
+let numeroAsteriscos = 5;
+let asterisco = "*";
+
+for(let linhaAsteriscos = 0; linhaAsteriscos < numeroAsteriscos; linhaAsteriscos ++) {
+  
+  let retornoAsterisco = "";
+  retornoAsterisco = asterisco.repeat(numeroAsteriscos);
+  console.log(`${retornoAsterisco}`);
+
+}
+
