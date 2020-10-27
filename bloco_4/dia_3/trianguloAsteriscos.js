6177d2b33158cc0b6086dc5f32636d513ebc868b
amandaos <amanda.775.souza@gmail.com>
2020-10-27 19:21:37 -0300

    trianguloAsterisco

---
+++ bloco_4/dia_3/trianguloAsteriscos.js
@@ -0,0 +1,9 @@
+let numeroAsteriscos = 5;
+let asterisco = "*";
+let retornoAsterisco = "";
+
+for(let linhaAsteriscos = 0; linhaAsteriscos <= numeroAsteriscos; linhaAsteriscos ++) {
+
+  retornoAsterisco = retornoAsterisco + asterisco;
+  console.log(`${retornoAsterisco}`);
+}
