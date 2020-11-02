8a9849abc2197d9dbe83bc08d3df8abed0d1bac6
amandaos <amanda.775.souza@gmail.com>
2020-11-01 22:13:50 -0300

    finaliza os exercicios

---
+++ bloco_4/dia_4/exercicio5.js
@@ -0,0 +1,23 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  };
+
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+  };
+  
+  for (key in info){
+      if ( info[key] === info.recorrente && info[key === "Sim" && info2[key] === "sim"]){
+        console.log("Ambos recorrentes");
+      } else {
+          console.log(info[key] + " e " + info2[key]);
+      } 
+  }
+ 
